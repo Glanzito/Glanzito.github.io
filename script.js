6833f9f96f749640d9132a6e2ff74107342d3353
Glanzito <manyikenkateko1@gmail.com>
2023-06-27 03:50:58 +0200

    upt

--- script.js
+++ script.js
@@ -35,7 +35,7 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["YouTuber", "Developer", "Photographer", "Designer", "Freelancer"],
+        strings: ["YouTuber", "Developer", "Cloud Associate", "Designer", "Freelancer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
