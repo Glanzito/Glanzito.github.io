46fb4ad08df2b107be83ec1c939a5dec8ddeda73
Glanzito <manyikenkateko1@gmail.com>
2026-03-13 09:33:53 +0200

    Just Updating
    update

--- script.js
+++ script.js
@@ -70,4 +70,14 @@ $(document).ready(function(){
             }
         }
     });
+    $(".owl-carousel").owlCarousel({
+    loop: true,
+    margin: 20,
+    nav: true,
+    responsive: {
+        0:    { items: 1 },
+        600:  { items: 2 },
+        1000: { items: 3 }
+    }
+});
 });
